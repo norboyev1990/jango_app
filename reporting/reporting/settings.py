cb2ff54bdbb137bc5e70ec9f599786986f5e9e42
Abdullobek <43066163+norboyev1990@users.noreply.github.com>
2020-06-02 12:41:27 +0500

    startapp credits

--- reporting/reporting/settings.py
+++ reporting/reporting/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static")
+)
