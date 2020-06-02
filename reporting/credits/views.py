f1ec269de7e758dc9360ebd64598745cfcaebbfb
Abdullobek <43066163+norboyev1990@users.noreply.github.com>
2020-06-02 14:39:17 +0500

    credits update

--- reporting/credits/views.py
+++ reporting/credits/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'credits/index.html'
+    context_object_name = 'latest_question_name'
--- reporting/reporting/settings.py
+++ reporting/reporting/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'credits.apps.CreditsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,8 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static")
-)
+STATIC_URL = '/static/'
