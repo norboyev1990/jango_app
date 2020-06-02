bc39e86b837e954b88261d994c7c23091f02bd8d
Abdullobek <norboyev1990@gmail.com>
2020-06-03 01:19:34 +0500

    create templates

--- reporting/credits/views.py
+++ reporting/credits/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
 
 # Create your views here.
-class IndexView(generic.ListView):
-    template_name = 'credits/index.html'
-    context_object_name = 'latest_question_name'
+def index(request):
+    return render(request, 'credits/index.html')
