4ef58c5d486b687421c62e402c2e7f0e50433a89
Abdullobek <norboyev1990@gmail.com>
2020-06-03 02:48:50 +0500

    adding static files

--- reporting/reporting/settings.py
+++ reporting/reporting/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '^=1g^q)f(i7h52$g(6do^6(&xry)o@_2@xt#1jehp80&1kfri#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -118,5 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
