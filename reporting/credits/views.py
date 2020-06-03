09e138394f3ef01c6e9bfe6b2ae40c15acf1244a
Abdullobek <43066163+norboyev1990@users.noreply.github.com>
2020-06-03 14:31:35 +0500

    All templates and includes are ready.

--- reporting/credits/views.py
+++ reporting/credits/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from openpyxl import load_workbook
+import pandas as pd
+from pandas import DataFrame
 # Create your views here.
 def index(request):
     return render(request, 'credits/index.html')
+def test(request):
+    #wb = load_workbook(filename = 'media/excel/january.xlsx')
+    #sheet_ranges = wb['report']
+    df = pd.read_excel (r'media/excel/january.xlsx')
+    context = {'mylist': df.to_html(encoding="true"), 'type': type(df)}
+    return render(request, 'credits/test.html', context)
