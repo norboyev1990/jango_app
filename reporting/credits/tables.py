167ead0f3064cfe5f7477b59bba463671985708a
Abdullobek <43066163+norboyev1990@users.noreply.github.com>
2020-06-09 12:26:02 +0500

    SQLITE succesfully!

--- reporting/credits/tables.py
+++ reporting/credits/tables.py
@@ -5,7 +5,8 @@ class ReportDataTable(tables.Table):
     class Meta:
         model = ReportData
         template_name = "django_tables2/bootstrap.html"
-        attrs = {"class": "table table-striped table-bordered"}
+        attrs = {"class": "table table-striped #table-bordered table-head-custom"}
+        orderable = False
         fields = (
             'UNIQUE_CODE',
             'BORROWER', 
