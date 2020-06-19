f4d29cebd68e5742c57f9bcb51513db19277d419
mika <mervert.kuchkarova@gmail.com>
2020-06-19 08:59:23 +0500

    lokalizaciya

--- reporting/credits/tables.py
+++ reporting/credits/tables.py
@@ -14,11 +14,12 @@ class ReportDataTable(tables.Table):
             'LOAN_BALANCE',
             )
 class OverallInfoTable(tables.Table):
-    name        = tables.Column()
-    old_value   = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    new_value   = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    difference  = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    percentage  = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
+    attr={"th":{"class":"text-right"}, "td":{"class":"text-right"}}
+    name        = tables.Column( verbose_name = "Показатели")
+    old_value   = tables.Column(attrs =attr, verbose_name = "Предыдущий  месяц")
+    new_value   = tables.Column(attrs =attr, verbose_name = "Текущий месяц")
+    difference  = tables.Column(attrs =attr, verbose_name = "Изменение")
+    percentage  = tables.Column(attrs =attr, verbose_name = "Изменение %")
     
     class Meta:
         template_name = "django_tables2/bootstrap.html"
@@ -29,15 +30,16 @@ class OverallInfoTable(tables.Table):
         self.base_columns[_column_name].verbose_name = _column_new_name 
 
 class ByTermTable(tables.Table):
+    attr = {"th":{"class":"text-right"}, "td":{"class":"text-right"}}
     name    = tables.Column()
-    portfel = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    ration = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    npl     = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    toxic   = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    npl_toxic  = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    weight  = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    rezerv  = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    coating  = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
+    portfel = tables.Column(attrs=attr)
+    ration = tables.Column(attrs=attr)
+    npl     = tables.Column(attrs=attr)
+    toxic   = tables.Column(attrs=attr)
+    npl_toxic  = tables.Column(attrs=attr)
+    weight  = tables.Column(attrs=attr)
+    rezerv  = tables.Column(attrs=attr)
+    coating  = tables.Column(attrs=attr)
 
 
     class Meta:
@@ -46,15 +48,16 @@ class ByTermTable(tables.Table):
         orderable = False
 
 class BySubjectTable(tables.Table):
-    TITLE       = tables.Column()
-    LOAN        = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    RATION      = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    NPL_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    TOX_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    TOX_NPL     = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    WEIGHT      = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    RESERVE     = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    COATING     = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
+    attr={"th":{"class":"text-right"}, "td":{"class":"text-right"}}
+    TITLE       = tables.Column(verbose_name="Статус")
+    LOAN        = tables.Column(attrs=attr, verbose_name="Кредитный портфель")
+    RATION      = tables.Column(attrs=attr, verbose_name="Доля %")
+    NPL_LOAN    = tables.Column(attrs=attr, verbose_name="NPL")
+    TOX_LOAN    = tables.Column(attrs=attr, verbose_name="Токсичные кредиты")
+    TOX_NPL     = tables.Column(attrs=attr, verbose_name="ТК+NPL")
+    WEIGHT      = tables.Column(attrs=attr, verbose_name="удельный вес к своему портфелю")
+    RESERVE     = tables.Column(attrs=attr, verbose_name="Резервы")
+    COATING     = tables.Column(attrs=attr, verbose_name="Покрытие ПК+NPL резервами")
 
     class Meta:
         template_name = "django_tables2/bootstrap.html"
@@ -62,15 +65,16 @@ class BySubjectTable(tables.Table):
         orderable = False
 
 class ByPercentageTable(tables.Table):
-    TITLE       = tables.Column()
-    ULL_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    ULL_PERCENT = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    ULS_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    ULS_PERCENT = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    FLL_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    FLL_PERCENT = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    FLS_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    FLS_PERCENT = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
+    attr={"th":{"class":"text-right"}, "td":{"class":"text-right"}}
+    TITLE       = tables.Column(verbose_name = "Коридор")
+    ULL_LOAN    = tables.Column(attrs=attr, verbose_name = "Долгосрочные ЮЛ")
+    ULL_PERCENT = tables.Column(attrs=attr, verbose_name = "Доля %")
+    ULS_LOAN    = tables.Column(attrs=attr, verbose_name = "Краткосрочные ЮЛ")
+    ULS_PERCENT = tables.Column(attrs=attr, verbose_name = "Доля %")
+    FLL_LOAN    = tables.Column(attrs=attr, verbose_name = "Долгосрочные ФЛ")
+    FLL_PERCENT = tables.Column(attrs=attr, verbose_name = "Доля %")
+    FLS_LOAN    = tables.Column(attrs=attr, verbose_name = "Краткосрочные ФЛ")
+    FLS_PERCENT = tables.Column(attrs=attr, verbose_name = "Доля %")
     
     class Meta:
         template_name = "django_tables2/bootstrap.html"
@@ -78,17 +82,18 @@ class ByPercentageTable(tables.Table):
         orderable = False
 
 class ByPercentageULTable(tables.Table):
-    TITLE       = tables.Column()
-    T2_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T2_PERCENT    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T5_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T5_PERCENT    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T7_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T7_PERCENT    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T10_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T10_PERCENT    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T11_LOAN    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    T11_PERCENT    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
+    attr={"th":{"class":"text-right"}, "td":{"class":"text-right"}}
+    TITLE       = tables.Column(verbose_name = "Коридор")
+    T2_LOAN    = tables.Column(attrs=attr, verbose_name = "до 2-х лет")
+    T2_PERCENT    = tables.Column(attrs=attr, verbose_name = "Доля %")
+    T5_LOAN    = tables.Column(attrs=attr, verbose_name = "от 2-х до 5 лет")
+    T5_PERCENT    = tables.Column(attrs=attr, verbose_name = "Доля %")
+    T7_LOAN    = tables.Column(attrs=attr, verbose_name = "от 5-ти до 7 лет")
+    T7_PERCENT    = tables.Column(attrs=attr, verbose_name = "Доля %")
+    T10_LOAN    = tables.Column(attrs=attr, verbose_name = "от 7-ми до 10 лет")
+    T10_PERCENT    = tables.Column(attrs=attr, verbose_name = "Доля %")
+    T11_LOAN    = tables.Column(attrs=attr, verbose_name = "свыше 10 лет")
+    T11_PERCENT    = tables.Column(attrs=attr, verbose_name = "Доля %")
     
     class Meta:
         template_name = "django_tables2/bootstrap.html"
@@ -96,11 +101,12 @@ class ByPercentageULTable(tables.Table):
         orderable = False
 
 class ByAverageULTable(tables.Table):
-    TITLE       = tables.Column()
-    UZS_AVERAGE    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    USD_AVERAGE    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    EUR_AVERAGE    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
-    JPY_AVERAGE    = tables.Column(attrs={"th":{"class":"text-right"}, "td":{"class":"text-right"}})
+    attr={"th":{"class":"text-right"}, "td":{"class":"text-right"}}
+    TITLE       = tables.Column(verbose_name = "Срок")
+    UZS_AVERAGE    = tables.Column(attrs=attr, verbose_name = "UZS")
+    USD_AVERAGE    = tables.Column(attrs=attr, verbose_name = "USD")
+    EUR_AVERAGE    = tables.Column(attrs=attr, verbose_name = "EUR")
+    JPY_AVERAGE    = tables.Column(attrs=attr, verbose_name = "JPY")
     
     class Meta:
         template_name = "django_tables2/bootstrap.html"
