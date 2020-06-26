bf6fff58c8666b8b423d096489abc24cb20e92b7
Abdullobek <43066163+norboyev1990@users.noreply.github.com>
2020-06-26 13:16:59 +0500

    startapp clients

--- reporting/reporting/urls.py
+++ reporting/reporting/urls.py
@@ -19,5 +19,6 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('credits/', include('credits.urls'))
+    path('credits/', include('credits.urls')),
+    path('clients/', include('clients.urls'))
 ]
